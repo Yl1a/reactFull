f7e23334e578ac17dae7ab5162e5ae149314c80f
Yl1a <volkova.ylia2004@gmail.com>
2024-02-19 17:56:46 +0300

    t7_1

--- sm2/src/component/data/data.jsx
+++ sm2/src/component/data/data.jsx
@@ -1,5 +1,5 @@
 export const CARDS = [
   { id: "1", title: "111", category: "111", price: "11111" },
-  { id: "2", title: "222", category: "222", price: "22222" },
-  { id: "3", title: "3333", category: "333", price: "3333" },
+  { id: "2", title: "21122", category: "222", price: "22222" },
+  { id: "3", title: "332233", category: "333", price: "3333" },
 ];
--- sm2/src/page/library/library.jsx
+++ sm2/src/page/library/library.jsx
@@ -1,7 +1,18 @@
 import Card from "../../component/card/card";
 import { CARDS } from "../../component/data/data";
+import { useState } from "react";
+
+import style from "./library.module.css";
 
 export default function Library() {
+  const [query, setQuery] = useState("");
+  function search(e) {
+    setQuery(e.target.value);
+  }
+
+  const filterItem = CARDS.filter((item) => {
+    return item.title.toLowerCase().includes(query.toLowerCase());
+  });
   return (
     <main className="main">
       <div className="container">
@@ -9,6 +20,19 @@ export default function Library() {
           <div className="container">
             <div className="main_content">
               <h2 className="title mainTitle">Книги</h2>
+              <div className="search">
+                <div className="form">
+                  <div className={style.search}>
+                    <input
+                      onChange={search}
+                      type="search"
+                      name="search"
+                      className={style.search_item}
+                      placeholder="search"
+                    />
+                  </div>
+                </div>
+              </div>
               <div className="category">
                 <div className="category_item">
                   <a href="" className="link">
@@ -32,6 +56,19 @@ export default function Library() {
                 </div>
               </div>
               <div className="cards">
+                {filterItem.map((item, id) => {
+                  return (
+                    <Card
+                      title={item.title}
+                      category={item.category}
+                      price={item.price}
+                      key={id}
+                      id={item.id}
+                    />
+                  );
+                })}
+                {/*
+                
                 {CARDS.map((item, id) => (
                   <Card
                     title={item.title}
@@ -40,7 +77,7 @@ export default function Library() {
                     key={id}
                     id={item.id}
                   />
-                ))}
+                ))} */}
               </div>
             </div>
           </div>
