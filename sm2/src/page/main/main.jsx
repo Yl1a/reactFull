d405e81501af8a3756793cb579360eb6d3e09936
Yl1a <volkova.ylia2004@gmail.com>
2024-02-24 23:09:20 +0300

    all

--- sm2/src/page/main/main.jsx
+++ sm2/src/page/main/main.jsx
@@ -1,4 +1,6 @@
 import Info from "/main/info.png";
+import SliderMain from "../../component/slider/slider";
+import Accordion from "../../component/accordion/accordion";
 
 export default function Main() {
   return (
@@ -9,8 +11,19 @@ export default function Main() {
             <img src={Info} alt="" className="img" />
             <div className="info_content">
               <h2 className="slogan">Leaf</h2>
-              <p className="text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore aliquid, placeat doloremque deleniti quos aliquam officiis maxime temporibus quia debitis ex? Et perspiciatis iure illum exercitationem eos facilis non sint.</p>
+              <p className="text">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
+                aliquid, placeat doloremque deleniti quos aliquam officiis
+                maxime temporibus quia debitis ex? Et perspiciatis iure illum
+                exercitationem eos facilis non sint.
+              </p>
             </div>
+          </div>
+          <SliderMain />
+          <div className="accordion">
+          <Accordion title="first" content="content first accordion"/>
+          <Accordion title="second" content="content second accordion"/>
+          <Accordion title="third" content="content third accordion"/>
           </div>
         </div>
       </div>
